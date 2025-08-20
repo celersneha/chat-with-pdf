61d4f19de5777e72339b3b71294d1b05f7701da1
Sneha Sharma <snehav2109@gmail.com>
2025-08-20 18:16:14 +0530

    feat: add RegisterUserOnSignIn component for user registration on sign-in

---
+++ client/src/components/RegisterUserOnSignIn.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function RegisterUserOnSignIn() {
+  const { user, isSignedIn } = useUser();
+
+  useEffect(() => {
+    if (isSignedIn && user) {
+      axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/user/register-user`, {
+        userId: user.id,
+        email: user.primaryEmailAddress?.emailAddress,
+        firstName: user.firstName,
+      });
+    }
+  }, [isSignedIn, user]);
+
+  return null;
+}
