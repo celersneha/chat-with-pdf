5fde236af9dd164787a420a50ffe0c8cf3c7aeea
Sneha Sharma <snehav2109@gmail.com>
2025-08-21 21:27:34 +0530

    create user service

---
+++ server/src/services/user.services.ts
@@ -0,0 +1,12 @@
+import { db } from "../db";
+import { users } from "../db/schema/user.schema";
+import { eq } from "drizzle-orm";
+
+export const getUserById = async (userId: string) => {
+  const user = await db.select().from(users).where(eq(users.userid, userId));
+  return user[0] || null;
+};
+
+export const canUploadFile = (user: any) => {
+  return (user?.uploadedFileCount ?? 0) < 5;
+};
