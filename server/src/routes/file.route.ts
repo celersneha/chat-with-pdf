829e2f8bd5dc2435e790ad5e7d89037c8bf6194f
Sneha Sharma <snehav2109@gmail.com>
2025-08-21 21:28:04 +0530

    feat: add file retrieval and deletion routes

--- server/src/routes/file.route.ts
+++ server/src/routes/file.route.ts
@@ -1,10 +1,16 @@
 import { requireAuth } from "@clerk/express";
-import { uploadFile } from "../controller/file.controller";
+import {
+  uploadFile,
+  getFilesOfCurrentUser,
+  deleteFile,
+} from "../controller/file.controller";
 import { Router } from "express";
 import upload from "../lib/multerStorage";
 
 const router = Router();
 
 router.route("/upload").post(requireAuth(), upload.single("pdf"), uploadFile);
+router.route("/get-files").get(requireAuth(), getFilesOfCurrentUser);
+router.route("/delete-file/:fileId").delete(requireAuth(), deleteFile);
 
 export default router;
