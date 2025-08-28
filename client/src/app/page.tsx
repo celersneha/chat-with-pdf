3e273527af9bf7c64d7c1bacd5ea4815ba389e9f
Sneha Sharma <snehav2109@gmail.com>
2025-08-28 20:19:50 +0530

    feat: wrap Home component in ClientApp

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,38 +3,34 @@ import FileUploadComponent from "../components/custom-components/FileUploadCompo
 import ChatComponent from "../components/custom-components/Chat";
 import UploadedFilesSidebar from "../components/custom-components/UploadedFiles";
 import { useRef } from "react";
+import ClientApp from "@/components/wrapper/ClientApp";
 
 export default function Home() {
   const sidebarRef = useRef<{ fetchUploadedFiles: () => void }>(null);
   return (
-    <div className="min-h-screen bg-gray-50 flex">
-      {/* Sidebar */}
-      <UploadedFilesSidebar sidebarRef={sidebarRef} />
+    <ClientApp>
+      <div className="min-h-screen bg-gray-50 flex">
+        {/* Sidebar */}
+        <UploadedFilesSidebar />
 
-      {/* Main content */}
-      <div className="flex-1 p-4">
-        <div className="max-w-4xl mx-auto space-y-6">
-          {/* Chat Window */}
-          <div className="bg-white rounded-lg shadow-lg border">
-            <ChatComponent />
-          </div>
+        {/* Main content */}
+        <div className="flex-1 p-4">
+          <div className="max-w-4xl mx-auto space-y-6">
+            {/* Chat Window */}
+            <div className="bg-white rounded-lg shadow-lg border">
+              <ChatComponent />
+            </div>
 
-          {/* Space between components */}
-          <div className="h-4"></div>
+            {/* Space between components */}
+            <div className="h-4"></div>
 
-          {/* File Upload Component */}
-          <div className="flex justify-center">
-            <FileUploadComponent
-              onUploadSuccess={() => {
-                // Add a small delay to let the worker process the file
-                setTimeout(() => {
-                  sidebarRef.current?.fetchUploadedFiles();
-                }, 1000); // Wait 1 second
-              }}
-            />
+            {/* File Upload Component */}
+            <div className="flex justify-center">
+              <FileUploadComponent />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ClientApp>
   );
 }
