fc35e90a9cae27aada92185a32c29aa9ca125709
Sneha Sharma <snehav2109@gmail.com>
2025-09-04 16:32:27 +0530

    feat: replace Home component content with LandingPage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,37 +4,8 @@ import ChatComponent from "../components/custom-components/Chat";
 import UploadedFilesSidebar from "../components/custom-components/UploadedFiles";
 import { useRef, useState } from "react";
 import ClientApp from "@/components/wrapper/ClientApp";
+import LandingPage from "@/components/landing-page/LandingPage";
 
 export default function Home() {
-  const sidebarRef = useRef<{ fetchUploadedFiles: () => void }>(null);
-  // Add this state to track selected files
-  const [selectedFileIds, setSelectedFileIds] = useState<string[]>([]);
-
-  return (
-    <ClientApp>
-      <div className="min-h-screen bg-gray-50 flex">
-        {/* Sidebar */}
-        <UploadedFilesSidebar onSelectionChange={setSelectedFileIds} />
-
-        {/* Main content */}
-        <div className="flex-1 p-4">
-          <div className="max-w-4xl mx-auto space-y-6">
-            {/* Chat Window */}
-            <div className="bg-white rounded-lg shadow-lg border">
-              {/* Pass selectedFileIds as prop */}
-              <ChatComponent selectedFileIds={selectedFileIds} />
-            </div>
-
-            {/* Space between components */}
-            <div className="h-4"></div>
-
-            {/* File Upload Component */}
-            <div className="flex justify-center">
-              <FileUploadComponent />
-            </div>
-          </div>
-        </div>
-      </div>
-    </ClientApp>
-  );
+  return <LandingPage />;
 }
