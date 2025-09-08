352e5c651cba4c21f34c6d6d1a2ee9eee3e4ccab
Sneha Sharma <snehav2109@gmail.com>
2025-09-08 13:12:50 +0530

    fix: update package.json scripts to use correct file paths and update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
-import fileRoutes from "./src/routes/file.route";
-import chatRoutes from "./src/routes/chat.route";
-import userRoutes from "./src/routes/user.route";
+import fileRoutes from "../src/routes/file.route";
+import chatRoutes from "../src/routes/chat.route";
+import userRoutes from "../src/routes/user.route";
 
 // Extend Express Request type to include 'auth'
 declare global {
@@ -49,3 +49,5 @@ app.use(
 app.listen(8000, () => {
   console.log("🚀 Server is running on http://localhost:8000");
 });
+
+export default app;
