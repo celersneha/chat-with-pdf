3b6b2018ff020c9f0a4d1d3c7944b480e3d91ba4
Sneha Sharma <snehav2109@gmail.com>
2025-09-08 22:48:37 +0530

    add upstash for production

--- server/src/utils/queue.ts
+++ server/src/utils/queue.ts
@@ -1,10 +1,18 @@
 import { Queue } from "bullmq";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const queue = new Queue("file-processing-queue", {
-  connection: {
-    host: "localhost",
-    port: 6379,
-  },
+  connection: isProd
+    ? {
+        host: process.env.UPSTASH_REDIS_REST_URL,
+        password: process.env.UPSTASH_REDIS_REST_TOKEN,
+        tls: {},
+      }
+    : {
+        host: "localhost",
+        port: 6379,
+      },
 });
 
 export default queue;
