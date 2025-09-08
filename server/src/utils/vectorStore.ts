cb060d9d7fb4aec50270298b1fd92ba0dc285c6f
Sneha Sharma <snehav2109@gmail.com>
2025-09-08 22:48:27 +0530

    add chromadb for production

--- server/src/utils/vectorStore.ts
+++ server/src/utils/vectorStore.ts
@@ -1,9 +1,17 @@
 import { QdrantVectorStore } from "@langchain/qdrant";
 import embeddings from "./embedding";
+import { Chroma } from "@langchain/community/vectorstores/chroma";
 
-const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
-  url: process.env.QDRANT_URL,
-  collectionName: "langchainjs-testing",
-});
+const isProd = process.env.NODE_ENV === "production";
+
+const vectorStore = isProd
+  ? new Chroma(embeddings, {
+      collectionName: "pdfiq",
+      chromaCloudAPIKey: process.env.CHROMA_API_KEY,
+    })
+  : await QdrantVectorStore.fromExistingCollection(embeddings, {
+      url: process.env.QDRANT_URL,
+      collectionName: "pdfiq",
+    });
 
 export default vectorStore;
