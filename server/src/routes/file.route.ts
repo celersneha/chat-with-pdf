a39ae02112cccbde311d05420ece8ffa1b61cc82
Sneha Sharma <snehav2109@gmail.com>
2025-09-09 12:23:51 +0530

    feat: add routes for processing files and deleting vector documents

--- server/src/routes/file.route.ts
+++ server/src/routes/file.route.ts
@@ -3,6 +3,8 @@ import {
   uploadFile,
   getFilesOfCurrentUser,
   deleteFile,
+  processFileReady,
+  processDeleteVectorDocs,
 } from "../controller/file.controller.js";
 import { Router } from "express";
 import upload from "../utils/multerStorage.js";
@@ -12,5 +14,9 @@ const router = Router();
 router.route("/upload").post(requireAuth(), upload.single("pdf"), uploadFile);
 router.route("/get-files").get(requireAuth(), getFilesOfCurrentUser);
 router.route("/delete-file/:fileId").delete(requireAuth(), deleteFile);
+
+//production
+router.post("/delete-vector-docs/:fileId", processDeleteVectorDocs);
+router.route("/process-file-ready").post(processFileReady);
 
 export default router;
