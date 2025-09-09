16b30d4aa825d97544b6087189f29e02fb94d03f
Sneha Sharma <snehav2109@gmail.com>
2025-09-09 23:45:46 +0530

    fix: update delete-vector-docs route to remove fileId parameter

--- server/src/routes/file.route.ts
+++ server/src/routes/file.route.ts
@@ -15,7 +15,7 @@ router.route("/get-files").get(requireAuth(), getFilesOfCurrentUser);
 router.route("/delete-file/:fileId").delete(requireAuth(), deleteFile);
 
 //production
-router.route("/delete-vector-docs/:fileId").post(processDeleteVectorDocs);
+router.route("/delete-vector-docs").post(processDeleteVectorDocs);
 router.route("/process-file-ready").post(processFileReady);
 
 export default router;
